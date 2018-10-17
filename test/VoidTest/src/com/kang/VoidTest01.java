45a6a59c188028d90fe669ca41d654a89733cd34
康建伟 <kangjianwei1990@qq.com>
2018-10-18 01:51:32 +0800

    :white_check_mark: 【java.lang.Void】 Void应用案例

---
+++ test/VoidTest/src/com/kang/VoidTest01.java
@@ -0,0 +1,16 @@
+package com.kang;
+
+public class VoidTest01 {
+    public static void main(String[] args) throws NoSuchMethodException {
+        String methodName = "fun";
+        
+        if(VoidTest01.class.getMethod("foo").getReturnType() == Void.TYPE) {
+            System.out.println(methodName + "方法的返回值为void");
+        } else {
+            System.out.println(methodName + "方法的返回值不为void");
+        }
+    }
+    
+    public void foo() {
+    }
+}
---
+++ test/VoidTest/src/com/kang/VoidTest02.java
@@ -0,0 +1,20 @@
+package com.kang;
+
+public class VoidTest02 {
+    public static void main(String[] args) {
+        Bar bar = new Bar();
+        bar.bar();
+    }
+}
+
+abstract class Foo<T> {
+    abstract T bar();
+}
+
+// 需要一个返回值为void的泛型
+class Bar extends Foo<Void> {
+    Void bar() {
+        System.out.println("Hello");
+        return null;
+    }
+}
