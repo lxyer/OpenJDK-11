f1639cbc638817491b82848cd7ae07a18327502e
康建伟 <kangjianwei1990@qq.com>
2018-10-28 00:07:44 +0800

    :bulb: 【java.util.function.UnaryOperator】 函数式接口：UnaryOperator<T>
    一元操作，继承了Function<T, T>
    
    参数：T
    返回：T

--- src/java/util/function/UnaryOperator.java
+++ src/java/util/function/UnaryOperator.java
@@ -37,15 +37,25 @@ package java.util.function;
  * @see Function
  * @since 1.8
  */
+/*
+ * 函数式接口：UnaryOperator<T>
+ *
+ * 一元操作，继承了Function<T, T>
+ *
+ * 参数：T
+ * 返回：T
+ */
 @FunctionalInterface
 public interface UnaryOperator<T> extends Function<T, T> {
-
+    
     /**
      * Returns a unary operator that always returns its input argument.
      *
      * @param <T> the type of the input and output of the operator
+     *
      * @return a unary operator that always returns its input argument
      */
+    // 标识转换（即自身到自身的转换）
     static <T> UnaryOperator<T> identity() {
         return t -> t;
     }
